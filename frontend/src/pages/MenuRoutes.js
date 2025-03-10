9a4009896f2c9ac0b33b798f09359ce641968323
Ahmad Hassan Khan <codewithahk@gmail.com>
2025-03-10 10:45:53 +0500

    feat:added lazy loading + fix: search css + loader

--- frontend/src/pages/MenuRoutes.js
+++ frontend/src/pages/MenuRoutes.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home/Home';
-import AddtoCart from '../components/AddtoCart/AddtoCart';
-import AboutUs from './About/AboutUs';
-import Categories from './Categories/Categories';
-import Contact from './Contact/Contact';
-import Login from './Login/Login';
-import CheckOut from '../components/CheckOut/CheckOut';
-import SearchShow from './SearchShow/SearchShow';
-import ForgetPassword from './ForgetPassword/ForgetPassword';
-import Address from './Address/Address';
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Loader from "./Loader/Loader";
+
+const Home = React.lazy(() => import("./Home/Home"));
+const AddtoCart = React.lazy(() => import("../components/AddtoCart/AddtoCart"));
+const AboutUs = React.lazy(() => import("./About/AboutUs"));
+const Categories = React.lazy(() => import("./Categories/Categories"));
+const Contact = React.lazy(() => import("./Contact/Contact"));
+const Login = React.lazy(() => import("./Login/Login"));
+const CheckOut = React.lazy(() => import("../components/CheckOut/CheckOut"));
+const SearchShow = React.lazy(() => import("./SearchShow/SearchShow"));
+const ForgetPassword = React.lazy(() =>
+  import("./ForgetPassword/ForgetPassword")
+);
+const Address = React.lazy(() => import("./Address/Address"));
 
 const MenuRoutes = () => {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/addtocart' element={<AddtoCart />} />
-        <Route path='/aboutus' element={<AboutUs />} />
-        <Route path='/category' element={<Categories />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/forgetpassword' element={<ForgetPassword />} />
-        <Route path='/checkout' element={<CheckOut />} />
-        <Route path='/search-results' element={<SearchShow />} />
-        <Route path='/address' element={<Address />} />
-        <Route path='/address2' element={<Address />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addtocart" element={<AddtoCart />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/category" element={<Categories />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgetpassword" element={<ForgetPassword />} />
+          <Route path="/checkout" element={<CheckOut />} />
+          <Route path="/search-results" element={<SearchShow />} />
+          <Route path="/address" element={<Address />} />
+          <Route path="/address2" element={<Address />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
